7218cc6341f2f10b2525641585571943330cc4c3
AnnaKostyukova <annakostyukova@coherentsolutions.com>
2022-07-04 00:53:28 +0300

    mp-4-sem-

---
+++ MP-4-sem-/Labs/Lab1/Lab1-1/Lab1-1/Lab1-1.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "Auxil.h"
+
+using namespace auxil;
+using namespace std;
+
+int main()
+{
+	start();
+	cout << dget(4.5, 9.5) << endl;
+	cout << iget(4, 9);
+}
+
